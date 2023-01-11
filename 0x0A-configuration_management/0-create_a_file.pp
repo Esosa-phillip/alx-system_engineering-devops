2194cc8a782b27d568ec12d2160c089539e7aef9
Esosa Omoigui <esosa.phillip@yahoo.com>
2023-01-11 23:36:56 +0100

    Creating a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 
 file { 'school':
   content => 'I love Puppet',
-  group => 'www-data',
-  mode => '0744',
-  owner => 'www-data',
-  path => '/tmp/school',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
